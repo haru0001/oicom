a93b26758b89cfd51fc571d15acd77eb188a6288
namizato <namizato@sinq.co.jp>
2019-12-02 00:56:41 +0900

    # ツイート関連追加
    - レイアウト変更
    - 画像追加

--- laravel/resources/views/dashboard/tweets/index.blade.php
+++ laravel/resources/views/dashboard/tweets/index.blade.php
@@ -3,29 +3,31 @@
 @section('content')
     <div class="content-wrapper">
         <div class="page-header">
-            <h3 class="page-title"> Form elements </h3>
+            <h3 class="page-title">追い込んだツイート一覧</h3>
         </div>
         <div class="row">
             @if (!empty($tweets))
                 @foreach ($tweets as $tweet)
-                    <div class="col-md-12 grid-margin stretch-card">
+                    <div class="col-md-6 offset-md-3 grid-margin stretch-card">
                         <div class="card">
-                            <div class="card-haeder p-3 w-100 d-flex">
-                            <img src="{{ $user_info['profile_image_url'] }}" class="rounded-circle" width="50" height="50">
+                            <div class="card-haeder d-flex p-4 w-100">
+                                <img src="{{ $user_info['profile_image_url'] }}" class="rounded-circle" width="50" height="50">
                                 <div class="ml-2 d-flex flex-column">
-                                    <p class="mb-0">{{ $user_info['name'] }}</p>
-                                    <a href="" class="text-secondary">{{ $user_info['screen_name'] }}</a>
-                                </div>
-                                <div class="d-flex justify-content-end flex-grow-1">
-                                    <p class="mb-0 text-secondary">{{ $tweet['created_at'] }}</p>
+                                    <p class="mb-0">{{ str_limit($user_info['name'], $limit = 30, $end = '...') }}</p>
+                                    <span class="text-secondary">{{ str_limit($user_info['screen_name'], $limit = 20, $end = '...') }}</span>
                                 </div>
                             </div>
                             <div class="card-body">
                                 {!! nl2br(e($tweet['text'])) !!}
                             </div>
-                            <div class="card-footer">
+                            <div class="card-footer bg-white text-right">
                                 @if ($tweet['cron'])
-                                    <p>{{ $tweet['cron']['reservation_at'] }}</p>
+                                    <span class="font-weight-bold text-secondary">追い込み予定日時</span>
+                                    <span class="font-weight-bold text-dark ml-2">{{ $tweet['cron']['reservation_at'] }}</span>
+                                @else
+                                    {{-- テスト --}}
+                                    <span class="font-weight-bold text-secondary">追い込み予定日時</span>
+                                    <span class="font-weight-bold text-dark ml-2">2019-12-31 00:00</span>
                                 @endif
                             </div>
                         </div>
