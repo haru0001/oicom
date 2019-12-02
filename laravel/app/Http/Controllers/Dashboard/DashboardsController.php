f7897a59d6ffb30b13d198bc5a869d0cedfb0cf2
namizato <namizato@sinq.co.jp>
2019-12-02 18:58:56 +0900

    # HOME画面の直近ツイート状況を削除

--- laravel/app/Http/Controllers/Dashboard/DashboardsController.php
+++ laravel/app/Http/Controllers/Dashboard/DashboardsController.php
@@ -11,10 +11,8 @@ class DashboardsController extends Controller
     public function index(Tweet $tweet)
     {
         $user_info = Auth::user();
-
         $tweet_count = $tweet->getTweetCount($user_info['id']);
-        $tweet_latest = $tweet->getTweetLatestDate($user_info['id']);
 
-        return view('dashboard.index', compact('user_info', 'tweet_count', 'tweet_latest'));
+        return view('dashboard.index', compact('user_info', 'tweet_count'));
     }
 }
