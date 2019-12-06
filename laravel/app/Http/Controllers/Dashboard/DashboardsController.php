2ce727cf09ba80794bf611c47516bfea43ed1559
namizato <namizato@sinq.co.jp>
2019-12-07 02:05:02 +0900

    # 管理画面のホームデザイン更新

--- laravel/app/Http/Controllers/Dashboard/DashboardsController.php
+++ laravel/app/Http/Controllers/Dashboard/DashboardsController.php
@@ -12,7 +12,24 @@ class DashboardsController extends Controller
     {
         $user_info = Auth::user();
         $tweet_count = $tweet->getTweetCount($user_info['id']);
+        $tweet_latest = $tweet->getTweetLatest($user_info['id']);
 
-        return view('dashboard.index', compact('user_info', 'tweet_count'));
+        if ($tweet_count <= 0) {
+            $rank = "自分に甘い腑抜け。常に養ってくれる人を求めている";
+        } elseif ($tweet_count <= 2) {
+            $rank = "期末テスト前の中学生位には頑張ってる";
+        } elseif ($tweet_count <= 4) {
+            $rank = "周りの目も変わってきた（良い意味なのかは分からない）";
+        } elseif ($tweet_count <= 8) {
+            $rank = "スタバでドヤ顔出来るくらいには意識高くなった";
+        } elseif ($tweet_count <= 10) {
+            $rank = "意識高すぎてもはやマルチっぽい";
+        } elseif ($tweet_count <= 15) {
+            $rank = "OICOMER（追い込マー）の称号を讃える";
+        } elseif ($tweet_count <= 30) {
+            $rank = "ここまで来るともう追い込んでるのか分からない";
+        }
+
+        return view('dashboard.index', compact('user_info', 'tweet_count', 'tweet_latest', 'rank'));
     }
 }
